f60bbc09afb41cf2b62e9c5b6cd88e934ac2651a
BoastfulApex <88885919+BoastfulApex@users.noreply.github.com>
2023-03-25 16:18:36 +0500

    posst method is added for test

--- apps/tours/views.py
+++ apps/tours/views.py
@@ -3,28 +3,28 @@ from .serialiers import *
 from rest_framework import generics
 
 
-class AboutView(generics.ListAPIView):
+class AboutView(generics.ListCreateAPIView):
     serializer_class = AboutSerializer
     
     def get_queryset(self):
         return About.objects.all()
     
     
-class ServiceView(generics.ListAPIView):
+class ServiceView(generics.ListCreateAPIView):
     serializer_class = ServiceSerializer
     
     def get_queryset(self):
         return Service.objects.all()
     
     
-class TourView(generics.ListAPIView):
+class TourView(generics.ListCreateAPIView):
     serializer_class = TourSerializer
     
     def get_queryset(self):
         return Tour.objects.all()
     
     
-class CommentView(generics.ListAPIView):
+class CommentView(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     
     def get_queryset(self):
